8bbdcd0a8cee10cd7a99b74d33bf8430000f68bc
Mauricio Yeudiel Cunjama Torres <qnhama@gmail.com>
2022-05-01 03:17:32 -0500

    Add project files.

---
+++ TurboPac/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TurboPac.Startup))]
+namespace TurboPac
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
